365725660db210d5d4c63dd4caabdccc0d95733f
Fausto Enrique <enriquefausto17@gmail.com>
2025-07-31 11:55:54 -0300

    Primer commit: agrego juego Amigo secreto básico

---
+++ app.js
@@ -0,0 +1,40 @@
+let amigos = [];
+
+function agregarAmigo() {
+  let input = document.querySelector("#amigo");
+  let nombre = input.value.trim();
+
+  if (nombre.length > 0) {
+    amigos.push(nombre);
+    alert(`Amigo ${nombre} agregado`);
+    input.value = "";
+
+    actualizarLista();
+  } else {
+    alert("Por favor, ingresa un nombre");
+  }
+  console.log(amigos);
+}
+
+function actualizarLista() {
+  let lista = document.getElementById("listaAmigos");
+  lista.innerHTML = "";
+
+  for (let i = 0; i < amigos.length; i++) {
+    let li = document.createElement("li");
+    li.textContent = amigos[i];
+    lista.appendChild(li);
+  }
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("No hay amigos para sortear. Por favor, agrega al menos uno.");
+        return;
+    }
+  let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+  let amigoSorteado = amigos[indiceAleatorio];
+
+  let resultado = document.getElementById("resultado");
+  resultado.innerHTML = `El amigo sorteado es: <strong>${amigoSorteado}<strong><li>`;
+}
